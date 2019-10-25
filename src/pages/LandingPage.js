28573bc9dcc21ee8565eb9376cf93ca7a310fac9
Sahil <sahilbondre@gmail.com>
2019-10-25 12:28:29 +0530

    Setup firebase firestore

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 import * as firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore"
 import { connect } from 'react-redux';
 
 import { changeIsUserSignedIn, setUser } from '../actions'
@@ -9,16 +10,20 @@ import { changeIsUserSignedIn, setUser } from '../actions'
 class LandingPage extends React.Component {
   handleSignIn = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
+    const db = firebase.firestore();
     provider.addScope('profile');
     firebase.auth().signInWithPopup(provider).then(result => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       // var token = result.credential.accessToken;
       // The signed-in user info.
       const user = result.user;
-      console.log(user)
       this.props.setUser(user);
       this.props.changeIsUserSignedIn(true);
-      console.log(this.props.user);
+      db.collection("users").doc(user.uid).set({
+        name: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL
+      })
       // ...
     })
   }
