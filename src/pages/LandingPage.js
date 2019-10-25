5178523fc0555f64aa39388eca656e5f55b82a18
Sahil <sahilbondre@gmail.com>
2019-10-25 11:20:50 +0530

    Add Oauth

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
 
-const LandingPage = () => {
-  return (
-    <div>
-      Landing Page
-    </div>
-  )
+class LandingPage extends React.Component {
+  handleSignIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      // var token = result.credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user)
+      // ...
+    }).catch(function (error) {
+      // Handle Errors here.
+      // var errorCode = error.code;
+      // var errorMessage = error.message;
+      // The email of the user's account used.
+      // var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      // var credential = error.credential;
+      // ...
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Button onClick={this.handleSignIn}>
+          Sign In
+        </Button>
+      </div>
+    )
+  }
 }
 
 export default LandingPage;
