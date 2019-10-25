5037c443c9142a64a9430e477ecd5c5400878143
Sahil <sahilbondre@gmail.com>
2019-10-25 10:56:06 +0530

    Initialize firebase config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from "firebase/app";
 
-import App from './components/App'
+import App from './App'
+import 'semantic-ui-css/semantic.min.css'
+import firebaseConfig from './config/firebase.config'
 
 ReactDOM.render(
   <App />,
   document.querySelector("#root")
 )
+
+firebase.initializeApp(firebaseConfig);
